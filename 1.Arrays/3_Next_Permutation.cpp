5610a168321c642299c7b800d287817fc7d8fde5
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-07 05:27:20 +0000

    Brute force approach Next Permutation

---
+++ 1.Arrays/3_Next_Permutation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n){
+
+    //  <----------------------------- Burte Force Approach ------------------------------------------>
+
+    // 1. Generate all possible Permutations of given array.
+    // 2. Find the given nums array in all permutations 
+    //     if present 
+    //        then return the next permutation right to it.
+    //     if num is not present
+    //        then return its first permutation.
+
+        //  _______________________________________
+        // |                                       |
+        // |  Time Complexity = O(N * N!)          |  //n elements and N! to respresent all permutations
+		// |  Space Complexity = O(N)              |  // to store permutation
+		// |_______________________________________|
+
+
+    
+}
