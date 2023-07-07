2989ab7fed79010d22b7993f76df7f799c65f5cd
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-07 12:14:07 +0000

    Better approach Sort array of 0's, 1's and 2's

--- 1.Arrays/5_Sort_an_array_of_0's_1's_&_2's.cpp
+++ 1.Arrays/5_Sort_an_array_of_0's_1's_&_2's.cpp
@@ -7,10 +7,62 @@ void sort012(int *nums, int n)
 
         //  __________________________________________
         // |                                          |
-        // |  Time Complexity = O(N*logN)             |  //running a single loop
+        // |  Time Complexity = O(N*logN)             |  // for sorting
 		// |  Space Complexity = O(1)                 |  // not using any extra space
 		// |__________________________________________|
 
 
-	sort(nums,nums+n);
+	// sort(nums,nums+n);
+
+    //<------------------------- Better Approach ----------------------------->
+
+    //-------------------- Keeping the Count of Values -----------------------------
+
+    // Intuition : There are only 3 distinct values so, it's easy to maintain the count of all values.
+    //             After that overwrite the array based on the count of values.
+
+    
+    // Step 1: Take 3 variable to maintain count of 0, 1 & 2.
+    // Step 2: Traverse the array once and incrementing the corresponding count variable.
+    // Step 3: Again traverse the array,Now over write the 
+    //             1st 'a' indexes with 0,
+    //             next 'b' indexes with 1,
+    //             and remaining 'c' indexes with 2.
+
+
+        //  __________________________________________
+        // |                                          |
+        // |  Time Complexity = O(N) + O(N)           |  //running 2 loops
+		// |  Space Complexity = O(1)                 |  // not using any extra space
+		// |__________________________________________|
+
+    //Step 1: Initialize the count variable for 0,1&2
+	int count_0 = 0, count_1 = 0, count_2 = 0;
+	//Step 2: Count the values and update their corresponding count variable
+	for(int i=0;i<n;i++){
+		if(nums[i]==0){
+			count_0++;
+		}
+		else if(nums[i]==1){
+			count_1++;
+		}
+		else{
+			count_2++;
+		}
+	}
+
+    //Step 3: Over write the array 
+	//Replacing 0
+	for(int i=0;i<count_0;i++){
+		nums[i] = 0;
+	}
+	//Replacing 1
+	for(int i=count_0;i<count_0+count_1;i++){
+		nums[i] = 1;
+	}
+	//Replacing 2
+	for(int i=count_0+count_1;i<n;i++){
+		nums[i] = 2;
+	}
+
 }
