e30d31c87cfc3bdb412637e8693e2b581aed937c
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-07 10:44:29 +0000

    Brute force Sort an array of 0's, 1's and 2's

---
+++ 1.Arrays/5_Sort_an_array_of_0's_1's_&_2's.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+void sort012(int *nums, int n)
+{
+    // <------------------------ Brute force approach ------------------------->
+
+    // Sorting -> Not the expected solution but it can be considered as one of the approach;
+
+        //  __________________________________________
+        // |                                          |
+        // |  Time Complexity = O(N*logN)             |  //running a single loop
+		// |  Space Complexity = O(1)                 |  // not using any extra space
+		// |__________________________________________|
+
+
+	sort(nums,nums+n);
+}
