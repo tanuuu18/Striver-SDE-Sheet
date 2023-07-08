6c08917fd7a5c46599bcf7a39372daa3da4b82a1
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-08 02:59:42 +0000

    update

--- 1.Arrays/5_Sort_an_array_of_0's_1's_&_2's.cpp
+++ 1.Arrays/5_Sort_an_array_of_0's_1's_&_2's.cpp
@@ -36,33 +36,51 @@ void sort012(int *nums, int n)
 		// |  Space Complexity = O(1)                 |  // not using any extra space
 		// |__________________________________________|
 
-    //Step 1: Initialize the count variable for 0,1&2
-	int count_0 = 0, count_1 = 0, count_2 = 0;
-	//Step 2: Count the values and update their corresponding count variable
-	for(int i=0;i<n;i++){
-		if(nums[i]==0){
-			count_0++;
-		}
-		else if(nums[i]==1){
-			count_1++;
-		}
-		else{
-			count_2++;
-		}
-	}
+    // //Step 1: Initialize the count variable for 0,1&2
+	// int count_0 = 0, count_1 = 0, count_2 = 0;
+	// //Step 2: Count the values and update their corresponding count variable
+	// for(int i=0;i<n;i++){
+	// 	if(nums[i]==0){
+	// 		count_0++;
+	// 	}
+	// 	else if(nums[i]==1){
+	// 		count_1++;
+	// 	}
+	// 	else{
+	// 		count_2++;
+	// 	}
+	// }
 
-    //Step 3: Over write the array 
-	//Replacing 0
-	for(int i=0;i<count_0;i++){
-		nums[i] = 0;
-	}
-	//Replacing 1
-	for(int i=count_0;i<count_0+count_1;i++){
-		nums[i] = 1;
-	}
-	//Replacing 2
-	for(int i=count_0+count_1;i<n;i++){
-		nums[i] = 2;
-	}
+    // //Step 3: Over write the array 
+	// //Replacing 0
+	// for(int i=0;i<count_0;i++){
+	// 	nums[i] = 0;
+	// }
+	// //Replacing 1
+	// for(int i=count_0;i<count_0+count_1;i++){
+	// 	nums[i] = 1;
+	// }
+	// //Replacing 2
+	// for(int i=count_0+count_1;i<n;i++){
+	// 	nums[i] = 2;
+	// }
+
+    //<------------------------- Optimal Approach ----------------------------->
+
+    //--------------------Dutch National Flag Algorithm -----------------------------
+
+	This algo contains 3 pointers: low, mid & high 
+
+	nums[0...low-1] contains 0. (Extreme left part)
+	nums[low...mid-1] contains 1. 
+	nums[high+1...n-1] contains 2. (Extreme right part)
+
+	nums[mid...high] (middle part) is the unsorted segment
+
+
+	1. Run a loop until mid<=high
+	2. There can be 3 different cases of mid pointer i.e. nums[mid]
+	    -> if(nums[mid]==0)
+		        then swap nums[low] and nums[mid]
 
 }
