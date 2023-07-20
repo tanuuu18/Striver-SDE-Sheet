0a30ea38a4b24378a4a7cd7b999aca29140920e3
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-20 03:44:24 +0000

    Rotate matrix Optimal approach

--- 1.Arrays/7_Rotate_Matrix.cpp
+++ 1.Arrays/7_Rotate_Matrix.cpp
@@ -20,15 +20,44 @@ void rotateMatrix(vector<vector<int>> &nums, int n, int m)
 		// |__________________________________________|
 
 
-   vector<vector<int>>rotated(n,vector<int>(m,0));
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			rotated[j][n-i-1] = nums[i][j];
-		}
-	}
-	nums = rotated;
+//    vector<vector<int>>rotated(n,vector<int>(n,0));
+// 	for(int i=0;i<n;i++){
+// 		for(int j=0;j<n;j++){
+// 			rotated[j][n-i-1] = nums[i][j];
+// 		}
+// 	}
+// 	nums = rotated;
    
 
+//    <----------------------- Optimal Approach ------------------------>
+    
+//    Intuition: By observation, 1st col of the original matrix is reverse of the 1st row of the rotated matrix.
+//               So that's why, we transpose the matric and reverse each row.
+// 			  Since, we are making changes in the matrix so space complexity reduced to O(1)
 
-       
+
+//    Step 1: Transpose the matrix.
+//    Step 2: reverse each row of the matrix.
+
+         //  __________________________________________
+        // |                                          |
+        // |  Time Complexity = O(N*N)                |  
+		// |  Space Complexity = O(1)                 | 
+		// |__________________________________________|
+
+
+
+	
+	//Step 1: Transpose the matrix
+    for(int i=0;i<n;i++){
+		for(int j=0;j<i;j++){
+			swap(nums[i][j],nums[j][i]);
+		}
+	}
+
+
+    // Step 2: reverse each row
+	for(int i=0;i<n;i++){
+		reverse(nums[i].begin(),nums[i].end());
+	}  
 }
