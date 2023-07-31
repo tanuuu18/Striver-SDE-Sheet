6c2a4498685937ab623966b421ab735d38372351
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-31 10:39:15 +0530

    7.Botton_View_Of_Binary_Tree.cpp

---
+++ 10.Binary Tree/7.Botton_View_Of_Binary_Tree.cpp
@@ -0,0 +1,27 @@
+vector <int> bottomView(Node *root) {
+       vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int>mp; // val,line
+        queue<pair<Node*,int>>q; // node,line
+        q.push({root,0});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            mp[line] = node->data;
+            if(node->left){
+                q.push({node->left,line-1});
+            }
+            if(node->right){
+                q.push({node->right,line+1});
+            }
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+            
+        }
+        return ans;
+    }
