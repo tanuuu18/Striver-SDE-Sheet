e9384a7137c8eb5ec957ecc2035790ff227b1a1f
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-31 13:49:02 +0530

    24.Symmetric_Binary_Tree.cpp

---
+++ 10.Binary Tree/24.Symmetric_Binary_Tree.cpp
@@ -0,0 +1,13 @@
+bool isSymmetricHelp(TreeNode* left , TreeNode* right){
+        if(left==NULL || right==NULL)
+            return left==right;
+        if(left->val != right->val)
+            return false;
+        return isSymmetricHelp(left->left,right->right)
+                            &&
+               isSymmetricHelp(left->right,right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return root==NULL || isSymmetricHelp(root->left,root->right);
+    }
+   
